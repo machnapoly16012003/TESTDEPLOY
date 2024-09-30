b439e4074810b06027bf39905aa4dcbb395ed1b1
Tran Phu Son <phuson2809@gmail.com>
2024-09-30 16:09:15 +0700

    update 3D

--- src/section/gettingStarted/Model.tsx
+++ src/section/gettingStarted/Model.tsx
@@ -12,8 +12,8 @@ export function Model(props) {
         makeDefault={true} // Đặt camera này làm camera chính
         far={3000} // Thay đổi giá trị far
         near={0.5} // Thay đổi giá trị near
-        fov={15} // Thay đổi giá trị fov
-        position={[-1000, 800, -1400]} // Thay đổi vị trí camera
+        fov={14} // Thay đổi giá trị fov
+        position={[-900, 1600, -1400]} // Thay đổi vị trí camera
         rotation={[0, -400, 300]} // Thay đổi góc quay camera
         scale={[1, 1, 1]} // Thay đổi tỷ lệ camera
       />
